d64eeb91cfe8a58a1067d1128dc45b3fb0fd846a
yaroslav.iv <yarikivanitsa39@gmail.com>
2025-07-21 16:51:26 +0100

    Search functionality implemented

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const MovieCard = ({movie}) => {
     return (
         <div className = "movie-card">
-            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`:
-                'no-Movie.png'}
+            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+            : 'no-movie.png'}
                 alt = {movie.title} 
             />
             <div className = "mt-4">
